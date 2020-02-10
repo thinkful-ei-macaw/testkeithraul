78951e42fa524a99216517862f749221103097c6
Keith Freitag <keithfreitag@gmail.com>
2020-02-10 15:32:18 -0800

    drills

---
+++ functionDrills1.js
@@ -0,0 +1,30 @@
+`use strict`
+
+function yearOfBirth(age) {
+    return 2020 - age;
+    }
+
+function createGreeting(name, age){
+    if (age < 0) {
+        throw new Error("Age can not be negative");
+    }
+    if (age === undefined || name === undefined) {
+        throw new Error("Arguments not valid");
+    }
+    if (typeof age !== 'number') {
+        throw new TypeError('Age needs to be a real number');
+    }
+   
+   
+    const dob = yearOfBirth(age);
+    return `Hi, my name is ${name} and I\'m ${age} years old.  I was born in ${dob}.`;
+}
+
+try {
+const greeting1 = createGreeting('Keith', 27);
+console.log(greeting1);
+}
+catch(e) {
+    console.error(e.message);
+}
+
---
+++ functionDrills2.js
@@ -0,0 +1,4 @@
+function jediName(firstName, lastName) {
+    const newName = 'slice.lastName(0,2) slice.firstName(0,1)
+    return newName;
+}
