46c746a704ccbcaa2711b9af05aeabe08d2e4eb6
Raul Garcia <punkvomit21@gmail.com>
2020-02-10 19:25:40 -0500

    infinity_comit

--- functionDrills2.js
+++ functionDrills2.js
@@ -1,6 +1,25 @@
-function jediName(firstName, lastName){
-  return lastName.slice(0,3) + firstName.slice(0,2);
-  
+function jediName(firstName, lastName) {
+  return lastName.slice(0, 3) + firstName.slice(0, 2);
+
 }
 
 console.log(jediName('Keith', 'Freitag'))
+
+//second part //
+
+function beyond(num) {
+  if (num === Infinity) {
+    return 'And beyond';
+  }
+  else if (num !== Infinity && num > 0) {
+    return 'To infinity';
+  }
+  else if (num !== Infinity && num < 0) {
+    return 'To negative infinity';
+  }
+  else if (num === 0) {
+    return 'staying home';
+  }
+}
+
+console.log(beyond(6));
