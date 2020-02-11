56b21a251c72bc802d7b5384c8c5e3e1635236e8
Keith Freitag <keithfreitag@gmail.com>
2020-02-10 16:46:02 -0800

    part 3

--- functionDrills2.js
+++ functionDrills2.js
@@ -23,3 +23,26 @@ function beyond(num) {
 }
 
 console.log(beyond(6));
+
+//third part
+
+function decode(encrypted) {
+    let words = encrypted.split(' ');
+    let result = '';
+    for(let i = 0; i < words.length; i++) {
+        if(words[i][0] === 'a') {
+            result += words[i][1];
+        } else if(words[i][0] === 'b'){
+            result += words[i][2];
+        } else if(words[i][0] === 'c') {
+            result += words [i][3];
+        } else if(words[i][0] === 'd') {
+            result += words[i][4];
+        } else {
+            result += ' ';
+        }
+        }
+
+return result;
+}
+console.log(decode('craft block argon meter bells brown croon droop'))
